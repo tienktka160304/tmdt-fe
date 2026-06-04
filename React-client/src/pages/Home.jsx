811392917f01ff7d5d8ae260ce3146ad06867fce
tienktka160304 <nvtien163@Gmail.com>
2026-06-04 14:30:41 +0700

    Update Home.jsx

--- React-client/src/pages/Home.jsx
+++ React-client/src/pages/Home.jsx
@@ -1,61 +1,59 @@
+import { useEffect } from "react";
+import BannerMain from "../component/home/banner/BannerMain";
 import CateHome from "../component/home/cateHome/CateHome";
 import HintForyour from "../component/home/hintForyou/HintForyour";
 import TopSale from "../component/home/topSale/TopSale";
+import TopBanChay from "../component/home/topBanChay/TopBanChay";
 import News from "../component/home/news/News";
 import Brands from "../component/home/brand/Brands";
+import AboutCPM from "../component/home/aboutCPM/AboutCPM";
 import UserReviews from "../component/home/userReview/UserReviews";
-import BannerMain from "../component/home/banner/BannerMain";
-import TopBanChay from "../component/home/topBanChay/TopBanChay";
 import ModalDetail from "../component/shared/ModalDetail";
 import { useSelector } from "react-redux";
-import { useEffect } from "react";
-import AboutCPM from "../component/home/aboutCPM/AboutCPM";
-import LoadingOverlay from "../component/shared/LoadingOverlay";
 import { HomeDataProvider } from "../context/HomeDataContext";
 
 export default function Home() {
   const showPopup = useSelector((state) => state.popupDetail.popUp);
+
   useEffect(() => {
-    // document.title = "Trang chủ | xedap_3s";
-    document.title = `Trang chủ | ${import.meta.env.VITE_WEBSITE_NAME}`;
+    document.title = `Trang chủ | ${import.meta.env.VITE_WEBSITE_NAME || "TMDT"}`;
   }, []);
+
   return (
     <HomeDataProvider>
-      <>
-        {/* <LoadingOverlay /> */}
+      <main className="overflow-hidden">
+        <BannerMain />
+        <CateHome />
+        <HintForyour />
 
-        <main className="overflow-hidden">
-          <BannerMain />
-          <CateHome />
-          <HintForyour />
-          <div className="flex items-center justify-center gap-4 bg-greenwhite py-4">
-            <div className="h-1 w-8 bg-gray-200"></div>
-            <div className="h-1 w-8 bg-gray-200"></div>
-            <div className="h-1 w-8 bg-gray-200"></div>
-            <div className="h-1 w-8 bg-gray-200"></div>
-          </div>
-          <TopSale />
-          <TopBanChay />
+        <div className="flex items-center justify-center gap-4 bg-greenwhite py-4">
+          <div className="h-1 w-8 bg-gray-200"></div>
+          <div className="h-1 w-8 bg-gray-200"></div>
+          <div className="h-1 w-8 bg-gray-200"></div>
+          <div className="h-1 w-8 bg-gray-200"></div>
+        </div>
 
-          <News />
-          <Brands />
-          <div className="flex items-center justify-center gap-4 py-4">
-            <div className="h-1 w-8 bg-gray-200"></div>
-            <div className="h-1 w-8 bg-gray-200"></div>
-            <div className="h-1 w-8 bg-gray-200"></div>
-            <div className="h-1 w-8 bg-gray-200"></div>
-          </div>
-          <AboutCPM />
+        <TopSale />
+        <TopBanChay />
+        <News />
+        <Brands />
 
-          <UserReviews />
-        </main>
+        <div className="flex items-center justify-center gap-4 py-4">
+          <div className="h-1 w-8 bg-gray-200"></div>
+          <div className="h-1 w-8 bg-gray-200"></div>
+          <div className="h-1 w-8 bg-gray-200"></div>
+          <div className="h-1 w-8 bg-gray-200"></div>
+        </div>
 
-        {showPopup && (
-          <div className="duration-300">
-            <ModalDetail />
-          </div>
-        )}
-      </>
+        <AboutCPM />
+        <UserReviews />
+      </main>
+
+      {showPopup && (
+        <div className="duration-300">
+          <ModalDetail />
+        </div>
+      )}
     </HomeDataProvider>
   );
 }
